8977bdbb7e33f7445244b19a87321b0d41d00827
Flowaria <wnsghgj1234@naver.com>
2021-01-01 01:17:47 +0900

    2021. 01. 01. - Minor Fix

--- GTFO.Custom.WardenObjective.Core/Utils/ChainedPuzzleContext.cs
+++ GTFO.Custom.WardenObjective.Core/Utils/ChainedPuzzleContext.cs
@@ -5,13 +5,13 @@ namespace GTFO.CustomObjectives.Utils
     public class ChainedPuzzleContext
     {
         public ChainedPuzzleInstance Instance { get; internal set; }
-        public string SolvedMessage { get; set; }
+        public string SolvedMessage { get; set; } = "Security Scan Complete, door unlocked.";
         public float SolvedMessageDuration { get; set; } = 10.0f;
         public ePUIMessageStyle SolvedMessageStyle { get; set; } = ePUIMessageStyle.Bioscan;
 
         internal void Solved()
         {
-            if (!string.IsNullOrWhiteSpace(SolvedMessage) || SolvedMessageDuration <= 0.0f)
+            if (!string.IsNullOrWhiteSpace(SolvedMessage) || SolvedMessageDuration >= 0.0f)
             {
                 GuiManager.InteractionLayer.SetTimedMessage(SolvedMessage, SolvedMessageDuration, SolvedMessageStyle, 0);
             }
